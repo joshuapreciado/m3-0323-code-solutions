7178c2faefc05baeecd3ff267d524fd9935d09b4
joshuapreciado <preciadojoshua340@yahoo.com>
2023-06-14 00:42:57 +0000

    React-routing

---
+++ react-routing/client/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Header from './components/Header';
+import Catalog from './pages/Catalog';
+import About from './pages/About';
+import ProductDetails from './pages/ProductDetails';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div><p>Hello</p></div>
+        <Routes>
+          <Route path='/' element={<Header />}>
+            <Route index element={<Catalog />} />
+            <Route path='/details/:productId' element={<ProductDetails />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </header>
+    </div>
+  );
+}
+
+export default App;
